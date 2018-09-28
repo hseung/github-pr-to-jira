22684322437a19f214712064269ce216e02b63d7
Dio Seung Heo <elondhs@gmail.com>
2018-09-28 16:33:32 +0900

    initial commit

---
+++ background.js
@@ -0,0 +1,11 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+	if (new URL(tab.url).hostname.indexOf('github') !== -1) {
+		var re = /\[[A-Z]*-\d*\]/;
+  		var found = tab.title.match(re)
+  		if (found.length > 0) {
+  			chrome.tabs.create({
+  				url: 'https://buzzvil.atlassian.net/browse/' + found[0].slice(1, -1)
+  			});
+  		}
+  	}
+});
