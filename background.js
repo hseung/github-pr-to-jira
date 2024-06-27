3533be9477db9b6c08ad15614e797d09b32dee63
ybbarng <ybbarng@gmail.com>
2024-06-27 16:09:10 +0900

    chrome.browserAction이 사라졌으므로 chrome.action으로 변경

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.action.onClicked.addListener(function(tab) {
 	if (new URL(tab.url).hostname.indexOf('github') !== -1) {
 		var re = /\[[A-Z]*-\d*\]/;
   		var found = tab.title.match(re)
